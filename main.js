28b75a4d3d76b8ea41bd49cd6b5754bde4a4119c
Robert Gorz <rjgorz@gmail.com>
2023-01-17 11:05:24 -0600

    implemented like button functionality with error catching

--- main.js
+++ main.js
@@ -3,8 +3,28 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector('div#modal');
+modal.className = 'hidden';
 
+const likebtns = document.querySelectorAll('.like-glyph');
 
+likebtns.forEach(likebtn => likebtn.addEventListener('click', () => {
+  mimicServerCall("someURL")
+  .then(() => {
+    if(likebtn.textContent === EMPTY_HEART) {
+      likebtn.textContent = FULL_HEART;
+      likebtn.className = 'activated-heart';
+    } else {
+      likebtn.textContent = EMPTY_HEART;
+      likebtn.className = '';
+    }
+  })
+  .catch((error) => {
+    modal.className = '';
+    modal.textContent = error;
+    setTimeout(() => modal.className = 'hidden', 3000);
+  })
+}));
 
 
 //------------------------------------------------------------------------------
